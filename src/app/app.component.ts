e0afd485a69acc5028d324f089bf5c31f21f173f
Lewis <castdeath11@gmail.com>
2021-05-24 15:37:40 +1200

    Init with UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {MatTableDataSource} from '@angular/material/table';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Reverb-JSK';
+  columnDefs = ['hz','RT','PF'];
+  dataSource = {
+    100: {RT: "1.23", PF: "1"},
+    125: {RT: "1.3", PF: "0"},
+    200: {RT: "13", PF: "0"},
+    250: {RT: "13", PF: "0"},
+    315: {RT: "13", PF: "0"},
+    400: {RT: "13", PF: "0"},
+    500: {RT: "13", PF: "0"},
+    630: {RT: "13", PF: "0"},
+    800: {RT: "13", PF: "0"},
+    1000: {RT: "13", PF: "0"},
+    1250: {RT: "13", PF: "0"},
+    1600: {RT: "13", PF: "0"},
+    2000: {RT: "13", PF: "0"},
+    2500: {RT: "13", PF: "0"},
+    3150: {RT: "13", PF: "0"},
+    4000: {RT: "13", PF: "0"},
+    5000: {RT: "13", PF: "0"},
+  };
+  logs = "UI Initialized";
+
+  test(){
+    console.log('YO');
+  }
+
+  add_message(message){
+    this.logs = message + "\n" + this.logs
+  }
+
+  trigger_measurements(){
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
